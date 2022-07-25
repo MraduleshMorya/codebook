d2573e3a117a140ac95a2f665e7a52a90a1e6030
Mradulesh Morya <76241036+MraduleshMorya@users.noreply.github.com>
2022-07-25 11:09:09 +0530

    Update settings.py

--- codebook/settings.py
+++ codebook/settings.py
@@ -206,4 +206,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mradulesh.morya@sofmen.com'
-EMAIL_HOST_PASSWORD = 'mradulesh9274'
+EMAIL_HOST_PASSWORD = '**********'
